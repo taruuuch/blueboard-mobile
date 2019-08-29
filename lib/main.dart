8237ba7a691727eff72cc0c3b3a475a03828c788
Artemchuk Dmytro <taruuuch@gmail.com>
2019-08-29 18:58:43 +0300

    Rename screens folder to pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:blueboard/screens/trip/trip_screen.dart';
-import 'package:blueboard/screens/intro/intro_screen.dart';
-import 'package:blueboard/screens/sign_up/signup_screen.dart';
-import 'package:blueboard/screens/sign_in/signin_screen.dart';
+import 'pages/trip/trip_screen.dart';
+import 'pages/intro/intro_screen.dart';
+import 'pages/sign_up/signup_screen.dart';
+import 'pages/sign_in/signin_screen.dart';
 
 var routes = <String, WidgetBuilder> {
   '/trip'  : (BuildContext context) => TripPage(),
--- lib/pages/sign_in/signin_screen.dart
+++ lib/pages/sign_in/signin_screen.dart
@@ -1,4 +1,4 @@
-import 'package:blueboard/screens/sign_in/widgets/signin_form.dart';
+import 'package:blueboard/pages/sign_in/widgets/signin_form.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
