71f4895d81a84f6ff7ff4332d27813d467b986d4
Artemchuk Dmytro <taruuuch@gmail.com>
2019-09-04 16:55:13 +0300

    Add user methods, interceptors to dio and methods for user data

---
+++ lib/models/user.dart
@@ -0,0 +1,30 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'user.g.dart';
+
+@JsonSerializable()
+class User {
+  User(
+		this.id, 
+		this.firstName, 
+		this.lastName,
+		this.status,
+		this.email, 
+		this.phone, 
+		this.password,
+		this.avatar
+	);
+
+  String id;
+  String firstName;
+  String lastName;
+	String email;
+	String phone;
+	String password;
+	String status;
+	Uri avatar;
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+
+  Map<String, dynamic> toJson() => _$UserToJson(this);
+}
