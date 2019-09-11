97448c1771a47acccbd164cba34e171484e026f5
Artemchuk Dmytro <taruuuch@gmail.com>
2019-09-12 00:33:26 +0300

    Create trip item ui and import to ListView

--- lib/pages/trips/trips_page.dart
+++ lib/pages/trips/trips_page.dart
@@ -1,6 +1,6 @@
-import 'package:blueboard/configs/app_style.dart';
 import 'package:blueboard/pages/trips/widgets/trips_bloc.dart';
-import 'package:blueboard/pages/trips/widgets/trips_item.dart';
+import 'package:blueboard/pages/trips/widgets/trips_list.dart';
+import 'package:blueboard/pages/trips/widgets/trips_search.dart';
 import 'package:blueboard/widgets/drawer/drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,22 +9,22 @@ class TripsPage extends StatelessWidget {
   static const String tag = 'trips';
 
   @override
-  Widget build(BuildContext context) =>
-    new Scaffold(
-      appBar: new AppBar(
-        title: new Text('TRIPS'),
-        centerTitle: true,
-        backgroundColor: Colors.white,
-      ),
-      drawer: AppDrawer(),
-      body: BlocProvider<TripsBloc> (
-        builder: (context) => TripsBloc(),
-        child: TripsPage(),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        tooltip: 'Create trip',
-        child: new Icon(Icons.add),
-        onPressed: () => null,
-      ),
-    );
+  Widget build(BuildContext context) => 
+		new Scaffold(
+			appBar: new AppBar(
+				title: new Text('TRIPS'),
+				centerTitle: true,
+				backgroundColor: Colors.white,
+			),
+			drawer: AppDrawer(),
+			body: BlocProvider<TripsBloc> (
+				builder: (context) => TripsBloc(),
+				child: new TripList()
+			),
+			floatingActionButton: new FloatingActionButton(
+				tooltip: 'Create trip',
+				child: new Icon(Icons.add),
+				onPressed: () => null,
+			),
+		);
 }
--- lib/providers/trips_provider.dart
+++ lib/providers/trips_provider.dart
@@ -1,24 +1,32 @@
 import 'dart:convert';
 import 'package:blueboard/configs/api_config.dart';
+import 'package:blueboard/models/trip.dart';
 import 'package:blueboard/providers/api_client.dart';
 
 class TripsProvider {
 	var _apiClient = ApiClient.instance;
 
-	Future get() async {
+	// TODO: getAll - return all user trips
+	Future getAll() async {
 		var response = await _apiClient.get(ApiConfig.tripUrl);
 		return response.data;
 	}
 
-	Future add() async {
-
+	Future add(Trip trip) async {
+		await _apiClient.post(ApiConfig.tripUrl, json.encode(trip.toJson()));
 	}
 
-	Future update() async {
-		
+	Future update(Trip trip) async {
+		await _apiClient.put(ApiConfig.tripUrl, json.encode(trip.toJson()));
 	}
 
-	Future delete() async {
+	// TODO: Get and delete single trip by ID
+	Future get(id) async {
+		var response = await _apiClient.get(ApiConfig.tripUrl + '/' + id);
+		return Trip.fromJson(response.data);
+	}
 
+	Future delete(id) async {
+		await _apiClient.delete(ApiConfig.tripUrl, id);
 	}
 }
