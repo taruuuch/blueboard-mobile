c5e47623875ce858af2edb64769051ab79d63792
Artemchuk Dmytro <taruuuch@gmail.com>
2019-09-24 10:46:12 +0300

    Create BaseTrip class to common fields with TripSlim

---
+++ lib/models/baseTrip.dart
@@ -0,0 +1,15 @@
+class BaseTrip {
+  String id;
+  String name;
+  String description;
+  String startDate;
+  String endDate;
+
+  BaseTrip({
+    this.id,
+    this.name,
+    this.description,
+    this.startDate,
+    this.endDate
+  });
+}
